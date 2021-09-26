e35d3a768cb9fe95aeae7f958756b64874329070
Rajeshkumar Ravichandran <rajeshkumar@mallow-tech.com>
2021-09-26 11:49:35 +0530

    Code alignment improvement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const isInteger = require('./src/isInteger');
-const findIndex = require('./src/findIndex');
-const find = require('./src/find');
+const isInteger = require("./src/isInteger");
+const findIndex = require("./src/findIndex");
+const find = require("./src/find");
 
-module.exports = {isInteger, findIndex, find};
+module.exports = { isInteger, findIndex, find };
